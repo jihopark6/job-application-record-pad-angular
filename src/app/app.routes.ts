9eecb778b3458a8196ed5f266e93b4fa4ac0d583
unknown <uk_admin@jprogramme.com>
2026-07-20 21:53:40 +0900

    Applying style, fixing errores, home components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ApplicationFormComponent } from './components/application-form/application-form.component';
+import { MemoComponent } from './components/memo/memo.component';
 
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+    { path: '', component: HomeComponent },
+    { path: 'new', component: ApplicationFormComponent },
+    { path: 'edit/:id', component: ApplicationFormComponent },
+    { path: 'memo', component: MemoComponent }
 ];
